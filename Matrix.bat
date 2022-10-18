86b91e13930e907d3ce6a09a690054a92ff22359
Destroyer9595 <111903804+Destroyer9595@users.noreply.github.com>
2022-10-18 21:19:02 +0200

    Add files via upload

---
+++ Matrix.bat
@@ -0,0 +1,7 @@
+@echo off
+color a
+mode 100
+title matrix
+:matrix
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+goto matrix
